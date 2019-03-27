4919ba053db7c25f5f4116fe1cd8bc18309cb396
Aman <amsingh714@gmail.com>
2019-03-27 13:42:45 -0400

    migrated roles table

--- migrations/20190327132837_roles_table.js
+++ migrations/20190327132837_roles_table.js
@@ -1,8 +1,17 @@
-
+// what new changes we need to make.
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTable("roles", function(tbl) {
+    // primary key, called it and make it auto-increment.
+    tbl.increments(); // defaults to a column named id - we could've done "role_id"
+
+    tbl
+      .string("name", 128)
+      .notNullable()
+      .unique();
+    });
 };
 
+// how to undo the changes made in the up function.
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('roles');
 };
